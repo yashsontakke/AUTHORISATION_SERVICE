e5401cc3f4e5c70490e506b6396ffd0ea29da183
Yash Shivajirao Sontakke <beingyash@Yashs-MacBook.local>
2023-07-11 16:46:27 +0530

    donw with many to many associations between user and role through user_roles

--- src/index.js
+++ src/index.js
@@ -2,19 +2,23 @@ const express = require("express");
 const app = express();
 const {PORT} = require("./config/serverConfig");
 const apiRouter = require('./router/apiRouter');
+const db = require("./models/index");
 
 
 const setupAndStartServer= async () =>{
     // Middleware for parsing JSON bodies
     app.use(express.json());
-
+    
     // Middleware for parsing URL-encoded bodies
     app.use(express.urlencoded({ extended: true }));
-    
+
     app.use('/api', apiRouter);
 
     app.listen(PORT, ()=>{
         console.log(`Server Statred At Port${PORT}`)
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter:true});
+        }
     })
 }
 
